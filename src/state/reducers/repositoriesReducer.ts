ea2c579ba8ccb45f8bee481d97b50ca7040cd56d
Dominik Manowski <manowski@protonmail.com>
2021-02-13 08:54:24 +0100

    Replace generic Action interface (payload?: any) with specific action interfaces

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -4,18 +4,31 @@ interface RepositoriesState {
   data: string[];
 }
 
-interface Action {
-  type: string;
-  payload?: any;
+interface SearchRepositoriesAction {
+  type: "search_repositories";
+}
+interface SearchRepositoriesSuccessAction {
+  type: "search_repositories_success";
+  payload: string[];
+}
+interface SearchRepositoriesErrorAction {
+  type: "search_repositories_error";
+  payload: string;
 }
 
-const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
+const reducer = (
+  state: RepositoriesState,
+  action:
+    | SearchRepositoriesAction
+    | SearchRepositoriesSuccessAction
+    | SearchRepositoriesErrorAction
+): RepositoriesState => {
   switch (action.type) {
     case "search_repositories":
       return { isLoading: true, error: null, data: [] };
-      case "search_repositories_success":
+    case "search_repositories_success":
       return { isLoading: false, error: null, data: action.payload };
-      case "search_repositories_error":
+    case "search_repositories_error":
       return { isLoading: true, error: action.payload, data: [] };
     default:
       return state;
