ffa41deb7d8a2d3717cd07bae594dd2a128d85dc
Dominik Manowski <manowski@protonmail.com>
2021-02-13 20:45:01 +0100

    Get state from store using useSelector  - state annotated as any

--- src/components/PackageList.tsx
+++ src/components/PackageList.tsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import { useActions } from "../hooks/useActions";
 
 const PackageList: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
   const { searchRepositories } = useActions();
-
+  const { data, isLoading, error } = useSelector(
+    (state: any) => state.repositories
+  );
+ 
   const handleSearchTermChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ): void => setSearchTerm(event.target.value);
@@ -19,7 +23,9 @@ const PackageList: React.FC = () => {
   return (
     <form onSubmit={onSearchSubmit}>
       <input type="text" onChange={handleSearchTermChange} value={searchTerm} />
-      <button type="submit">Search</button>
+      <button type="submit">
+        Search
+      </button>
     </form>
   );
 };
