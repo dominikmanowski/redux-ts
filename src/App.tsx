91fa85e20340ce304b0a70dbd89348b2f680c086
Dominik Manowski <manowski@protonmail.com>
2021-02-13 19:22:09 +0100

    Create PackageList component and add it to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Provider } from "react-redux";
 import { store } from "./state";
+import PackageList from "./components/PackageList";
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <div>
         <h1>Search for a Package</h1>
+        <PackageList />
       </div>
     </Provider>
   );
---
+++ src/components/PackageList.tsx
@@ -0,0 +1,10 @@
+const PackageList: React.FC = () => {
+  return (
+    <form>
+      <input type="text"/>
+      <button>Search</button>
+    </form>
+  );
+};
+
+export default PackageList;
