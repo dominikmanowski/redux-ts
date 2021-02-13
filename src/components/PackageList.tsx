e28cb37fbcb928c901d942020d950a695998f9d0
Dominik Manowski <manowski@protonmail.com>
2021-02-13 19:53:05 +0100

    Add onChange handler for search input and onSubmit handlar for from

--- src/components/PackageList.tsx
+++ src/components/PackageList.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 const PackageList: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchTermChange = (event: React.ChangeEvent<HTMLInputElement>): void =>
+    setSearchTerm(event.target.value);
+
+  const onSearchSubmit = (event: React.FormEvent<HTMLFormElement>): void =>
+    event.preventDefault();
+
   return (
-    <form>
-      <input type="text"/>
-      <button>Search</button>
+    <form onSubmit={onSearchSubmit}>
+      <input type="text" onChange={handleSearchTermChange} value={searchTerm} />
+      <button type="submit">Search</button>
     </form>
   );
 };
