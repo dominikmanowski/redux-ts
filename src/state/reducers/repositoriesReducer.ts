f645cda0659e80186b7f642459f27ee032308a97
Dominik Manowski <manowski@protonmail.com>
2021-02-13 17:17:11 +0100

    Move actions to separate file in state/actions/

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,25 +1,10 @@
+import { Action } from "../actions";
+
 interface RepositoriesState {
   isLoading: boolean;
   error: string | null;
   data: string[];
 }
-
-interface SearchRepositoriesAction {
-  type: ActionType.SEARCH_REPOSITORIES;
-}
-interface SearchRepositoriesSuccessAction {
-  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
-  payload: string[];
-}
-interface SearchRepositoriesErrorAction {
-  type: ActionType.SEARCH_REPOSITORIES_ERROR;
-  payload: string;
-}
-
-type Action =
-  | SearchRepositoriesAction
-  | SearchRepositoriesSuccessAction
-  | SearchRepositoriesErrorAction;
 
 enum ActionType {
   SEARCH_REPOSITORIES = "search_repositories",
