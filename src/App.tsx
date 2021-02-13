373543a0dbfc3b943db9d87eb60003072943c275
Dominik Manowski <manowski@protonmail.com>
2021-02-13 19:11:10 +0100

    Connect App component to store via Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Provider } from "react-redux";
+import { store } from "./state";
+
 const App: React.FC = () => {
-  return <h1>Redux with TypeScript</h1>;
-}
+  return (
+    <Provider store={store}>
+      <div>
+        <h1>Search for a Package</h1>
+      </div>
+    </Provider>
+  );
+};
 
 export default App;
