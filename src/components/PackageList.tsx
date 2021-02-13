a4222ecf638724ff04940d6593669c090bf97277
Dominik Manowski <manowski@protonmail.com>
2021-02-13 20:13:19 +0100

    Add acrtion dispatch handling with useDispatch hook

--- src/components/PackageList.tsx
+++ src/components/PackageList.tsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { actionCreators } from "../state";
 
 const PackageList: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
-  const handleSearchTermChange = (event: React.ChangeEvent<HTMLInputElement>): void =>
-    setSearchTerm(event.target.value);
+  const dispatch = useDispatch();
 
-  const onSearchSubmit = (event: React.FormEvent<HTMLFormElement>): void =>
+  const handleSearchTermChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => setSearchTerm(event.target.value);
+
+  const onSearchSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
+
+    dispatch(actionCreators.SearchRepositories(searchTerm));
+  };
 
   return (
     <form onSubmit={onSearchSubmit}>
